ba083da9dfc8da4ab2a1ba8085e7ab7fd00304de
Connor Sullivan <cksullvn@gmail.com>
2020-07-12 21:23:45 -0500

    initial component setup complete for front end

--- InsideIntervals.ui/src/App/App.js
+++ InsideIntervals.ui/src/App/App.js
@@ -1,13 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+// import firebase from 'firebase';
+import Navbar from '../components/shared/Navbar/Navbar';
+import Home from '../components/pages/Home/Home';
+import Intervals from '../components/pages/Intervals/Intervals';
+import UserProfile from '../components/pages/UserProfile/UserProfile';
+import Login from '../components/shared/Login/Login';
 
-function App() {
-  return (
+import './App.scss';
+import fbConnection from '../helpers/data/connection';
+// import userData from '../helpers/data/userData';
+
+fbConnection();
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true ? <Component {...props} {...rest}/> : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />);
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+class App extends React.Component {
+  state = {
+    authed: false,
+    registeredUser: {},
+    firebaseUser: {},
+  }
+
+  render() {
+    const { authed, registeredUser } = this.state;
+    return (
     <div className="App">
-      <button className="btn btn-danger">Bootstrap Button</button>
+      <Router>
+        <Navbar authed={authed} registeredUserId={registeredUser?.id} />
+        <Switch>
+          <Route path="/" exact component={Home} authed={authed} />
+          <Route
+            path='/login'
+            render={() => (authed ? (
+                <Redirect to='/home' />
+            ) : (
+                <Login />
+            ))
+            }
+          />
+          <Route path="/Intervals" exact component={Intervals} authed={authed} />
+          <PrivateRoute path="/userProfile" exact component={UserProfile} authed={authed} userObj={registeredUser}/>
+        </Switch>
+      </Router>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ InsideIntervals.ui/src/components/shared/Navbar/Navbar.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import logo from '../../../images/insideIntervalsLogo.PNG';
+import './Navbar.scss';
+// import Login from '../../shared/Login/Login';
+// import fbConnection from '../../../helpers/data/connection';
+
+// fbConnection();
+
+class Navbar extends React.Component {
+  static propTypes = {
+    userAuthed: PropTypes.bool,
+  }
+
+  logUserOut = (e) => {
+    e.preventDefault();
+    // TBD
+  }
+
+  render() {
+    const { userAuthed } = this.props;
+    const buildAuthedNav = () => {
+      if (userAuthed) {
+        return (
+
+          <div className="collapse navbar-collapse order-2" id="navbarText2">
+          <ul className="navbar-nav ml-auto nav2">
+            <li className="nav-item">
+              <a className="nav-link" href="/profile">Profile</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="TBD">Logout</a>
+            </li>
+          </ul>
+        </div>
+        );
+      }
+      return (
+        <div className="collapse navbar-collapse order-2" id="navbarText2">
+          <ul className="navbar-nav ml-auto nav2">
+            <li className="nav-item">
+              <a className="nav-link" href="/login">Login</a>
+            </li>
+          </ul>
+        </div>
+      );
+    };
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark">
+        <a className="navbar-brand" href="/">
+          <img src={logo} width="250" alt="Inside Intervals"/>
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse order-1" id="navbarText">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Get Started<span className="sr-only">(current)</span></a>
+            </li>
+          </ul>
+        </div>
+            { buildAuthedNav() }
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
