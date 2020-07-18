de3bce8ecc2a2ca8dd60bd332542a2e04001127b
Connor Sullivan <cksullvn@gmail.com>
2020-07-17 23:17:27 -0500

    created ui for Intervals ed component, and now working to connect the front end and database before testing full functionality of Intervals education component. also updated properties setup of some classes under the Models folder of the backend to coincide with updates to the database

---
+++ InsideIntervals.api/Models/IntervalShape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsideIntervals.api.Models
+{
+    public class IntervalShape
+    {
+        public int ShapeId { get; set; }
+        public int IntervalId { get; set; }
+        public string AscendingOrDescending { get; set; }
+        public byte[] ShapeImgFile { get; set; }
+    }
+}
--- InsideIntervals.api/Models/Mode.cs
+++ InsideIntervals.api/Models/Mode.cs
@@ -8,8 +8,10 @@ namespace InsideIntervals.api.Models
     public class Mode
     {
         public int ModeId { get; set; }
+        public string ModeType { get; set; }
         public string ModeName { get; set; }
         public string ModeDescription { get; set; }
         public byte[] ModeImg { get; set; }
+        public string UniqueIntervals { get; set; }
     }
 }
